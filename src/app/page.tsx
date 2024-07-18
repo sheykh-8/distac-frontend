fe1cc1d305f9fdd0a3b425424f56db386415fcef
Ali Sheykh <sheykha8@gmail.com>
2024-07-18 18:52:20 +0330

    initial commit for Distts system user interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -108,6 +109,7 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <Button>Hi Guys</Button>
     </main>
   );
 }
