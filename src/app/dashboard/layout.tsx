a129748131259843eac7ac194aae88df76da6a74
Ali Sheykh <sheykha8@gmail.com>
2024-07-20 07:04:02 +0330

    minor update

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,28 @@
+"use client"
+
+import { LoadingSpinner } from "@/components/loading-spinner";
+import { useFirebase } from "@/hooks/useFirebase";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <TooltipProvider>{children}</TooltipProvider>;
+  const { auth } = useFirebase();
+  const [user, loading, error] = useAuthState(auth);
+
+  const router = useRouter();
+
+  if (!user) {
+    router.replace("/");
+  }
+
+  if (loading) {
+    return <LoadingSpinner />
+  }
+
+  if (user) {
+    return <TooltipProvider>{children}</TooltipProvider>;
+  }
+
+  return null;
 }
